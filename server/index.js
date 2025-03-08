65b59c94871738dace2b5e90e9ee66447f3b5eb8
TelleQuelle <atticmaxim@gmail.com>
2025-03-08 15:34:19 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,6 +27,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self'");
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    res.setHeader('Pragma', 'no-cache');
     console.log('CSP updated to allow self, scripts, styles, and images');
     next();
 });
